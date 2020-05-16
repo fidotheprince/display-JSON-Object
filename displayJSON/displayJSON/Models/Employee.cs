6c100f12239a9d59f72c42dc413f9acac03a75fc
Alvaro Castro-Cid <alvarocastrocid@Alvaros-MacBook-Pro.local>
2020-05-16 10:01:05 -0700

    started to create table

--- displayJSON/displayJSON/Models/Employee.cs
+++ displayJSON/displayJSON/Models/Employee.cs
@@ -6,7 +6,6 @@ namespace displayJSON.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
-        public string Address { get; set; }
  
     }
 }
